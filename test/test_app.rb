92ee8165b50c553ed154ce6e35aa3e1754df70fe
Lucas Mendelowski <lucas@mendelowski.com>
2025-07-16 22:54:46 +0200

    Rename test helper

--- test/test_app.rb
+++ test/test_app.rb
@@ -1,12 +1,12 @@
-require_relative 'test_helper'
+require_relative 'helper'
 
 class TestApp < Minitest::Test
   def test_root_endpoint
     get '/'
-    
+
     assert last_response.ok?
     assert_equal 'application/json', last_response.content_type
-    
+
     data = JSON.parse(last_response.body)
     assert_equal 'Hacker News MCP Server', data['message']
     assert_equal '1.0.0', data['version']
@@ -15,10 +15,10 @@ class TestApp < Minitest::Test
 
   def test_health_endpoint
     get '/health'
-    
+
     assert last_response.ok?
     assert_equal 'application/json', last_response.content_type
-    
+
     data = JSON.parse(last_response.body)
     assert_equal 'ok', data['status']
     assert data['timestamp'].is_a?(Integer)
